4fe54a8e518f996c9fc02783bd03185e1c340b05
Marcus <marcus1337>
2024-12-02 14:41:18 +0100

    refactor

--- src/models.ts
+++ src/models.ts
@@ -1,10 +1,44 @@
+import * as THREE from 'three';
+import { FileCollections } from './assets';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
-export enum Models{
+export enum Models {
     OpticalTable
 }
 
-export const modelFilepaths: { [key in Models]: string } = {
+const modelFilepaths: { [key in Models]: string } = {
     [Models.OpticalTable]: "objs/optical_table.obj",
 };
 
+function applyStandardMaterial(obj: THREE.Object3D) {
+    obj.traverse((child) => {
+        if ((child as THREE.Mesh).isMesh) {
+            const mesh = child as THREE.Mesh;
+            if (!mesh.material) {
+                mesh.material = new THREE.MeshStandardMaterial({ color: 0x888888 });
+            }
+        }
+    });
+}
 
+function loadOBJ(collection: FileCollections, filepath: string): THREE.Object3D | null {
+    const objFile = collection.textFiles.get(filepath);
+    let obj = null;
+    if (objFile) {
+        const loader = new OBJLoader();
+        obj = loader.parse(objFile);
+        applyStandardMaterial(obj);
+    }
+    return obj;
+}
+
+export function loadModels(collection: FileCollections): Map<Models, THREE.Object3D> {
+    let modelMap: Map<Models, THREE.Object3D> = new Map();
+    const keys: Models[] = [Models.OpticalTable];
+    for (const key of keys) {
+        var obj = loadOBJ(collection, modelFilepaths[key]);
+        if (obj)
+            modelMap.set(key, obj);
+    }
+    return modelMap;
+}
--- src/sceneSetup.ts
+++ src/sceneSetup.ts
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { FileCollections } from './assets';
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { Models, modelFilepaths } from "./models"
+import { Models, loadModels } from "./models"
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 function getCamera(): THREE.PerspectiveCamera {
@@ -12,11 +11,9 @@ function getCamera(): THREE.PerspectiveCamera {
 
 function getScene(): THREE.Scene {
   const scene = new THREE.Scene();
-
   const light = new THREE.DirectionalLight(0xffffff, 1);
   light.position.set(5, 5, 5).normalize();
   scene.add(light);
-
   return scene
 }
 
@@ -24,41 +21,7 @@ function getRenderer(): THREE.WebGLRenderer {
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-
   return renderer;
-}
-
-function applyStandardMaterial(obj: THREE.Object3D) {
-  obj.traverse((child) => {
-    if ((child as THREE.Mesh).isMesh) {
-      const mesh = child as THREE.Mesh;
-      if (!mesh.material) {
-        mesh.material = new THREE.MeshStandardMaterial({ color: 0x888888 });
-      }
-    }
-  });
-}
-
-function loadOBJ(collection: FileCollections, filepath: string): THREE.Object3D | null {
-  const objFile = collection.textFiles.get(filepath);
-  let obj = null;
-  if (objFile) {
-    const loader = new OBJLoader();
-    obj = loader.parse(objFile);
-    applyStandardMaterial(obj);
-  }
-  return obj;
-}
-
-function loadModels(collection: FileCollections): Map<Models,THREE.Object3D> {
-  let modelMap: Map<Models,THREE.Object3D> = new Map();
-  const keys: Models[] = [Models.OpticalTable];
-  for (const key of keys){
-    var obj = loadOBJ(collection, modelFilepaths[key]);
-    if(obj)
-      modelMap.set(key, obj);
-  }
-  return modelMap;
 }
 
 export class SceneSetup {
