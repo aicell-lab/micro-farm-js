be55481a3a4f2da64bfaa4bbbd75db5c4d38269e
Marcus <marcus1337>
2025-01-10 15:38:06 +0100

    refactor

--- src/system/sceneSystem.ts
+++ src/system/sceneSystem.ts
@@ -1,10 +1,11 @@
+import * as THREE from 'three';
 import { CameraController } from './cameraController';
 import { SimulationLoop } from './simLoop';
 import { Room, createScene } from '../setup/room';
 import { ActorController } from './actorController';
 import { RenderController } from './renderController';
-import * as THREE from 'three';
 import { RoomActors } from '../setup/actor';
+import { FrameTime } from '../types/frameTime';
 
 function populateScene(scene: THREE.Scene, actors: RoomActors, room: Room): void {
   scene.add(actors.player.mesh);
@@ -12,27 +13,43 @@ function populateScene(scene: THREE.Scene, actors: RoomActors, room: Room): void
   scene.add(room.opticalTable);
 }
 
+function getFrameTime(prevFrameTime?: FrameTime): FrameTime {
+  const timestamp = performance.now();
+  const delta = prevFrameTime ? (timestamp - prevFrameTime.timestamp) / 1000.0 : 0;
+  return { delta, timestamp };
+}
+
 export class SceneSystem {
 
   private actorController: ActorController;
   private cameraController: CameraController;
+  private renderController: RenderController;
   private simLoop: SimulationLoop;
+  private frameTime: FrameTime;
 
   constructor(room: Room, actors: RoomActors) {
     let scene = createScene();
     populateScene(scene, actors, room);
     this.cameraController = new CameraController(actors.player.mesh);
-    let renderController = new RenderController(scene, this.cameraController.getCamera());
-    this.simLoop = new SimulationLoop(room, actors, renderController);
+    this.renderController = new RenderController(scene, this.cameraController.getCamera());
+    this.simLoop = new SimulationLoop(room, actors);
     this.actorController = new ActorController(actors);
+    this.frameTime = getFrameTime();
   }
 
   simulationLoop = () => {
+    const updatedFrameTime = getFrameTime(this.frameTime);
     this.cameraController.executeWithOffsetHandling(() => {
-      this.actorController.handleUserInput();
-      this.simLoop.step();
+      this.processFrame();
     });
+    this.frameTime = updatedFrameTime
     requestAnimationFrame(this.simulationLoop);
   };
+
+  processFrame() {
+    this.actorController.handleUserInput();
+    this.simLoop.step(this.frameTime.delta);
+    this.renderController.render();
+  }
 
 }
--- src/system/simLoop.ts
+++ src/system/simLoop.ts
@@ -1,44 +1,29 @@
-import { FrameTime } from '../types/frameTime';
 import { Room } from '../setup/room';
 import { RoomActors } from '../setup/actor';
-import { RenderController } from './renderController';
 
 interface SimState {
     actors: RoomActors;
     room: Room;
 }
 
-export function simPhysicsStep(state: SimState, frameTime: FrameTime): void {
-    state.actors.player.update(frameTime.delta);
-}
-
-function getFrameTime(prevFrameTime?: FrameTime): FrameTime {
-    const timestamp = performance.now();
-    const delta = prevFrameTime ? (timestamp - prevFrameTime.timestamp) / 1000.0 : 0;
-    return { delta, timestamp };
+export function simPhysicsStep(state: SimState, delta: number): void {
+    state.actors.player.update(delta);
 }
 
 export class SimulationLoop {
-    private frameTime: FrameTime;
     private room: Room;
     private actors: RoomActors;
-    private renderController: RenderController;
 
-    constructor(room: Room, actors: RoomActors, renderController: RenderController) {
-        this.frameTime = getFrameTime();
+    constructor(room: Room, actors: RoomActors) {
         this.room = room;
         this.actors = actors;
-        this.renderController = renderController
     }
 
     getSimState(): SimState {
         return { actors: this.actors, room: this.room }
     }
 
-    step(): void {
-        const updatedFrameTime = getFrameTime(this.frameTime);
-        simPhysicsStep(this.getSimState(), this.frameTime);
-        this.renderController.render();
-        this.frameTime = updatedFrameTime
+    step(delta: number): void {
+        simPhysicsStep(this.getSimState(), delta);
     }
 } 
