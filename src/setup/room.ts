8ebb683a9453efe14df09a6d2d6b3c2ca463e87f
Marcus <marcus1337>
2025-01-20 11:25:42 +0100

    comment out obj table

--- src/setup/room.ts
+++ src/setup/room.ts
@@ -2,6 +2,5 @@ import { RoomObject } from '../object/roomObject';
 
 export interface Room {
     floor: RoomObject;
-    opticalTable: RoomObject;
 }
 
--- src/setup/roomObjectFactory.ts
+++ src/setup/roomObjectFactory.ts
@@ -1,9 +1,9 @@
-import * as THREE from 'three';
-import { FLOOR_Y_POSITION, TABLE_ROTATION_DEGREES } from "./constants";
+//import * as THREE from 'three';
+//import { FLOOR_Y_POSITION, TABLE_ROTATION_DEGREES } from "./constants";
+//import { OpticalTable } from '../object/opticalTable';
+//import { MathUtils } from 'three';
 import { RoomObject } from '../object/roomObject';
-import { OpticalTable } from '../object/opticalTable';
 import { Floor } from '../object/floor';
-import { MathUtils } from 'three';
 import { Room } from './room';
 
 
@@ -17,25 +17,26 @@ export class RoomObjectFactory {
         return new Floor();
     }
 
-    createOpticalTable(): RoomObject {
+    /*createOpticalTable(): RoomObject {
         let table = new OpticalTable();
         setFloorPosition(table.object);
         table.object.rotation.x = MathUtils.degToRad(TABLE_ROTATION_DEGREES);
         return table;
-    }
+    }*/
 
     createRoom(): Room {
         return {
-            floor: this.createFloor(), opticalTable: this.createOpticalTable()
+            floor: this.createFloor()
         }
     }
 
 }
 
-function setFloorPosition(object: THREE.Object3D) {
+/*function setFloorPosition(object: THREE.Object3D) {
     const boundingBox = new THREE.Box3().setFromObject(object);
     const minY = boundingBox.min.y;
     if (minY < FLOOR_Y_POSITION) {
         object.position.y -= minY;
     }
-}
+}*/
+
