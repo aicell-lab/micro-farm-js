2573d48b9b15098c915c63575b82088383d1cc07
Marcus <marcus1337>
2025-01-29 11:44:38 +0100

    add sky

--- src/setup/sceneFactory.ts
+++ src/setup/sceneFactory.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { Room } from './room';
 import { RoomActors } from '../actor/roomActors';
+import { Sky } from 'three/examples/jsm/Addons.js';
 
 export class SceneFactory {
     private room: Room;
@@ -11,10 +12,25 @@ export class SceneFactory {
         this.actors = actors;
     }
 
+    createSky(): Sky {
+        const sky = new Sky();
+        sky.scale.setScalar(10000);
+        const sun = new THREE.Vector3();
+        const phi = THREE.MathUtils.degToRad(90);
+        const theta = THREE.MathUtils.degToRad(0);
+        sun.setFromSphericalCoords(1, phi, theta);
+        sky.material.uniforms['rayleigh'].value = 0.95;
+        sky.material.uniforms['mieCoefficient'].value = 0.0;
+        sky.material.uniforms['mieDirectionalG'].value = 0.0;
+        sky.material.uniforms.sunPosition.value.copy(sun);
+        return sky;
+    }
+
     createScene(): THREE.Scene {
         const scene = new THREE.Scene();
         createLights().forEach((light) => scene.add(light));
         this.populateScene(scene);
+        scene.add(this.createSky());
         return scene
     }
 
@@ -30,7 +46,7 @@ function createLights(): THREE.Light[] {
     const dirLight = new THREE.DirectionalLight(0xffffff, 1);
     dirLight.position.set(5, 5, 5).normalize();
     const ambientLight = new THREE.AmbientLight(0x404040, 0.5);
-    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x222222, 0.2); 
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x222222, 0.2);
     return [dirLight, ambientLight, hemiLight];
 }
 
