8268b4c049d698abcc594a1bdccac87e6ce745fa
Marcus <marcus1337>
2025-02-04 14:43:31 +0100

    rm frametime

--- src/system/sceneSystem.ts
+++ src/system/sceneSystem.ts
@@ -4,17 +4,10 @@ import { SimulationLoop } from './simulationLoop';
 import { Room } from '../setup/room';
 import { ActorController } from './actorController';
 import { RenderController } from './renderController';
-import { FrameTime } from '../types/frameTime';
 import { RoomActors } from '../actor/roomActors';
 import { DashboardController } from './dashboardController';
 import { InputListener } from '../io/input';
 import { CollisionController } from './collisionController';
-
-function getFrameTime(prevFrameTime?: FrameTime): FrameTime {
-  const timestamp = performance.now();
-  const delta = prevFrameTime ? (timestamp - prevFrameTime.timestamp) / 1000.0 : 0;
-  return { delta, timestamp };
-}
 
 export class SceneSystem {
   private collisionController: CollisionController;
@@ -23,7 +16,7 @@ export class SceneSystem {
   private cameraController: CameraController;
   private renderController: RenderController;
   private simulationLoop: SimulationLoop;
-  private frameTime: FrameTime;
+  private clock: THREE.Clock;
 
   constructor(room: Room, actors: RoomActors, scene: THREE.Scene) {
     this.cameraController = new CameraController(actors.player.object);
@@ -31,25 +24,23 @@ export class SceneSystem {
     this.renderController = new RenderController(scene, this.cameraController.getCamera());
     this.actorController = new ActorController(actors, new InputListener(this.dashboardController));
     this.simulationLoop = new SimulationLoop(room, actors);
-    this.frameTime = getFrameTime();
+    this.clock = new THREE.Clock();
     this.collisionController = new CollisionController();
     this.collisionController.addObject(room.cube.object);
     this.collisionController.addObject(actors.player.object);
   }
 
   runSimulationLoop = () => {
-    const updatedFrameTime = getFrameTime(this.frameTime);
     this.cameraController.executeWithOffsetHandling(() => {
       this.processNextFrame();
     });
-    this.frameTime = updatedFrameTime
     requestAnimationFrame(this.runSimulationLoop);
   };
 
   processNextFrame() {
     this.collisionController.checkCollisions();
     this.actorController.handleUserInput();
-    this.simulationLoop.step(this.frameTime.delta);
+    this.simulationLoop.step(this.clock.getDelta());
     this.renderController.render();
   }
 
--- src/types/frameTime.ts
+++
@@ -1,4 +0,0 @@
-export interface FrameTime {
-    delta: number;
-    timestamp: number;
-}
