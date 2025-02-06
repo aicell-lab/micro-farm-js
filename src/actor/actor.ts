0f02e37cf0255b02bcba65cc96915965dd4643fb
Marcus <marcus1337>
2025-02-06 13:29:11 +0100

    refactor

--- src/actor/actor.ts
+++ src/actor/actor.ts
@@ -1,25 +1,5 @@
 import * as THREE from 'three';
-import { MeshStandardMaterial } from 'three';
 import { PlayerController } from './playerController';
-
-function createDefaultMaterial(): MeshStandardMaterial {
-    const material = new MeshStandardMaterial({
-        color: 0x008822,
-        metalness: 0.3,
-        roughness: 0.7
-    });
-    return material;
-}
-
-export function createDefaultActorMesh(): THREE.Object3D {
-    let object = new THREE.Object3D();
-    const geometry = new THREE.BoxGeometry(0.5, 1.0, 0.5);
-    const material = createDefaultMaterial();
-    let mesh = new THREE.Mesh(geometry, material);
-
-    object.add(mesh);
-    return object;
-}
 
 export abstract class Actor {
     object: THREE.Object3D;
--- src/actor/human.ts
+++ src/actor/human.ts
@@ -1,5 +1,26 @@
-import { Actor, createDefaultActorMesh } from './actor';
+import { Actor } from './actor';
 import { PlayerController } from './playerController';
+import * as THREE from 'three';
+import { MeshStandardMaterial } from 'three';
+
+function createDefaultMaterial(): MeshStandardMaterial {
+    const material = new MeshStandardMaterial({
+        color: 0x008822,
+        metalness: 0.3,
+        roughness: 0.7
+    });
+    return material;
+}
+
+function createDefaultActorMesh(): THREE.Object3D {
+    let object = new THREE.Object3D();
+    const geometry = new THREE.BoxGeometry(0.5, 1.0, 0.5);
+    const material = createDefaultMaterial();
+    let mesh = new THREE.Mesh(geometry, material);
+
+    object.add(mesh);
+    return object;
+}
 
 export class Human extends Actor {
 
