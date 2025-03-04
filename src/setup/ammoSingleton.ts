8f87d1ffffb32f7dc7c161ae24f43af7ce6c52c7
Marcus <marcus1337>
2025-03-04 10:35:23 +0100

    refactor

--- src/setup/ammoSingleton.ts
+++ src/setup/ammoSingleton.ts
@@ -1,16 +1,19 @@
+type AmmoType = typeof import("ammojs-typed").default;
 
 export class AmmoSingleton {
-    private static instance: typeof import("ammojs-typed").default | null = null;
-    static async init() {
+    private static instance: AmmoType | null = null;
+
+    static async init(): Promise<void> {
         if (!this.instance) {
-            this.instance = (await (await import("ammojs-typed")).default()) as typeof import("ammojs-typed").default;
+            const AmmoModule = await import("ammojs-typed");
+            this.instance = (await AmmoModule.default()) as AmmoType;
         }
     }
-    static get() {
+
+    static get(): AmmoType {
         if (!this.instance) {
-            throw new Error("Ammo has not been initialized. Call AmmoSingleton.init() first.");
+            throw new Error("Ammo.js has not been initialized. Call AmmoSingleton.init() first.");
         }
         return this.instance;
     }
 }
-
--- src/system/physicsWorld.ts
+++ src/system/physicsWorld.ts
@@ -2,137 +2,31 @@ import * as THREE from 'three';
 import Ammo from 'ammojs-typed';
 import { AmmoSingleton } from '../setup/ammoSingleton';
 
-/*function changeObjectColor(object: THREE.Object3D, color: THREE.Color | string | number) {
-    object.traverse((child) => {
-        if (child instanceof THREE.Mesh && child.material) {
-            if (Array.isArray(child.material)) {
-                child.material.forEach((mat) => {
-                    mat.color.set(color);
-                    mat.needsUpdate = true;
-                });
-            } else {
-                child.material.color.set(color);
-                child.material.needsUpdate = true;
-            }
-        }
-    });
-}*/
-
-/*class CollisionHandler {
-    private objectMap: Map<number, THREE.Object3D>;
-    private originalMaterials: Map<THREE.Object3D, THREE.Material | THREE.Material[]> = new Map();
-    private collidingObjects: Set<THREE.Object3D> = new Set();
-
-    constructor(objectMap: Map<number, THREE.Object3D>) {
-        this.objectMap = objectMap;
-    }
-
-    detectCollisions(world: Ammo.btDiscreteDynamicsWorld) {
-        const newColliding = new Set<THREE.Object3D>();
-
-        const dispatcher = world.getDispatcher();
-        const numManifolds = dispatcher.getNumManifolds();
-        for (let i = 0; i < numManifolds; i++) {
-            const contactManifold = dispatcher.getManifoldByIndexInternal(i);
-            const bodyA = contactManifold.getBody0() as Ammo.btRigidBody;
-            const bodyB = contactManifold.getBody1() as Ammo.btRigidBody;
-            const numContacts = contactManifold.getNumContacts();
-            let collisionDetected = false;
-
-            for (let j = 0; j < numContacts; j++) {
-                const pt = contactManifold.getContactPoint(j);
-                if (pt.getDistance() < 0) {
-                    collisionDetected = true;
-                    break;
-                }
-            }
-
-            if (collisionDetected) {
-                const objectA = this.objectMap.get(bodyA.getUserIndex());
-                const objectB = this.objectMap.get(bodyB.getUserIndex());
-                const collisionColor = new THREE.Color(0.5, 0.5, 0.5);
-
-                if (objectA) {
-                    newColliding.add(objectA);
-                    changeObjectColor(objectA, collisionColor);
-                }
-                if (objectB) {
-                    newColliding.add(objectB);
-                    changeObjectColor(objectB, collisionColor);
-                }
-            }
-        }
-
-        this.restorePreviousState(newColliding);
-        this.collidingObjects = newColliding;
-    }
-
-    private restorePreviousState(newColliding: Set<THREE.Object3D>) {
-        this.collidingObjects.forEach((object) => {
-            if (!newColliding.has(object)) {
-                this.restoreOriginalMaterial(object);
-            }
-        });
-    }
-
-    private storeOriginalMaterial(object: THREE.Object3D) {
-        object.traverse((child) => {
-            if (child instanceof THREE.Mesh && child.material) {
-                if (!this.originalMaterials.has(child)) {
-                    if (Array.isArray(child.material)) {
-                        this.originalMaterials.set(child, child.material.map(mat => mat.clone()));
-                    } else {
-                        this.originalMaterials.set(child, child.material.clone());
-                    }
-                }
-            }
-        });
-    }
-
-    private restoreOriginalMaterial(object: THREE.Object3D) {
-        object.traverse((child) => {
-            if (child instanceof THREE.Mesh && this.originalMaterials.has(child)) {
-                const originalMaterial = this.originalMaterials.get(child)!;
-
-                if (Array.isArray(originalMaterial)) {
-                    child.material = originalMaterial.map(mat => mat.clone());
-                } else {
-                    child.material = originalMaterial.clone();
-                }
-
-                child.material.needsUpdate = true;
-            }
-        });
-    }
-
-    public registerObject(object: THREE.Object3D) {
-        this.storeOriginalMaterial(object);
-    }
-}*/
+function createPhysicsWorld(): Ammo.btDiscreteDynamicsWorld {
+    const Ammo = AmmoSingleton.get();
+    let collisionConfiguration = new Ammo.btDefaultCollisionConfiguration(),
+        dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration),
+        overlappingPairCache = new Ammo.btDbvtBroadphase(),
+        solver = new Ammo.btSequentialImpulseConstraintSolver();
+    const world = new Ammo.btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+    return world;
+}
 
 export class PhysicsWorld {
     private world: Ammo.btDiscreteDynamicsWorld;
     private rigidBodies: Ammo.btRigidBody[] = [];
     private objectMap: Map<number, THREE.Object3D> = new Map();
     private nextUserIndex = 0;
-    //private collisionHandler: CollisionHandler;
 
     constructor() {
+        this.world = createPhysicsWorld();
         const Ammo = AmmoSingleton.get();
-        let collisionConfiguration = new Ammo.btDefaultCollisionConfiguration(),
-            dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration),
-            overlappingPairCache = new Ammo.btDbvtBroadphase(),
-            solver = new Ammo.btSequentialImpulseConstraintSolver();
-        this.world = new Ammo.btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
         this.world.setGravity(new Ammo.btVector3(0, -10, 0));
-
-        //this.collisionHandler = new CollisionHandler(this.objectMap);
     }
 
     step(dt: number) {
         const maxSubSteps = 10;
         this.world.stepSimulation(dt, maxSubSteps);
-        //this.collisionHandler.detectCollisions(this.world);
     }
 
     addRigidBody(body: Ammo.btRigidBody, object: THREE.Object3D): void {
@@ -141,6 +35,5 @@ export class PhysicsWorld {
         this.objectMap.set(userIndex, object);
         this.rigidBodies.push(body);
         this.world.addRigidBody(body);
-        //this.collisionHandler.registerObject(object);
     }
 }
