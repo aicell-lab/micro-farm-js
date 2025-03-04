3536432d32d6e5e025222db32a02a1401a299a3e
Marcus <marcus1337>
2025-03-04 17:19:02 +0100

    refactor

--- src/system/physicsSystem.ts
+++ src/system/physicsSystem.ts
@@ -3,47 +3,7 @@ import * as THREE from 'three';
 import { EntityCollection } from '../setup/entityCollection';
 import Ammo from 'ammojs-typed';
 import { AmmoSingleton } from '../setup/ammoSingleton';
-
-function createBoxShape(object: THREE.Mesh): Ammo.btBoxShape {
-    const Ammo = AmmoSingleton.get();
-
-    const bbox = new THREE.Box3().setFromObject(object);
-    const size = new THREE.Vector3();
-    bbox.getSize(size);
-    const halfExtents = new Ammo.btVector3(size.x / 2, size.y / 2, size.z / 2);
-    const shape = new Ammo.btBoxShape(halfExtents);
-
-    return shape;
-}
-
-function createBody(object: THREE.Mesh, mass: number): Ammo.btRigidBody {
-    const Ammo = AmmoSingleton.get();
-    const transform = new Ammo.btTransform();
-    transform.setIdentity();
-    transform.setOrigin(new Ammo.btVector3(
-        object.position.x,
-        object.position.y,
-        object.position.z
-    ));
-
-    const motionState = new Ammo.btDefaultMotionState(transform);
-    const shape = createBoxShape(object);
-    const localInertia = new Ammo.btVector3(0, 0, 0);
-    if (mass > 0) shape.calculateLocalInertia(mass, localInertia);
-    const rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
-    return new Ammo.btRigidBody(rbInfo);
-}
-
-function getMesh(object: THREE.Object3D): THREE.Mesh {
-    if (object instanceof THREE.Mesh) {
-        return object;
-    }
-    const mesh = object.children.find(child => child instanceof THREE.Mesh) as THREE.Mesh | undefined;
-    if (mesh) {
-        return mesh;
-    }
-    throw new Error("No Mesh found in object");
-}
+import { AmmoUtils } from './physicsUtil';
 
 export class PhysicsSystem {
     private entities: EntityCollection;
@@ -90,17 +50,17 @@ export class PhysicsSystem {
     }
 
     private addMesh(mesh: THREE.Mesh, mass: number): void {
-        this.objects.set(mesh, createBody(mesh, mass));
+        this.objects.set(mesh, AmmoUtils.createBody(mesh, mass));
         this.masses.set(mesh, mass);
     }
 
     private addObject(object: THREE.Object3D, mass: number): void {
-        const mesh = getMesh(object);
+        const mesh = AmmoUtils.getMesh(object);
         this.addMesh(mesh, mass);
     }
 
     private getRigidBody(object: THREE.Object3D): Ammo.btRigidBody {
-        const mesh = getMesh(object);
+        const mesh = AmmoUtils.getMesh(object);
         const body = this.objects.get(mesh);
         if (!body) throw new Error("Body not found");
         return body;
---
+++ src/system/physicsUtil.ts
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+import Ammo from 'ammojs-typed';
+import { AmmoSingleton } from '../setup/ammoSingleton';
+
+export namespace AmmoUtils {
+    export function createBoxShape(object: THREE.Mesh): Ammo.btBoxShape {
+        const Ammo = AmmoSingleton.get();
+        const bbox = new THREE.Box3().setFromObject(object);
+        const size = new THREE.Vector3();
+        bbox.getSize(size);
+        const halfExtents = new Ammo.btVector3(size.x / 2, size.y / 2, size.z / 2);
+        return new Ammo.btBoxShape(halfExtents);
+    }
+
+    export function createBody(object: THREE.Mesh, mass: number): Ammo.btRigidBody {
+        const Ammo = AmmoSingleton.get();
+        const transform = new Ammo.btTransform();
+        transform.setIdentity();
+        transform.setOrigin(new Ammo.btVector3(
+            object.position.x,
+            object.position.y,
+            object.position.z
+        ));
+
+        const motionState = new Ammo.btDefaultMotionState(transform);
+        const shape = createBoxShape(object);
+        const localInertia = new Ammo.btVector3(0, 0, 0);
+        if (mass > 0) shape.calculateLocalInertia(mass, localInertia);
+        const rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
+        return new Ammo.btRigidBody(rbInfo);
+    }
+
+    export function getMesh(object: THREE.Object3D): THREE.Mesh {
+        if (object instanceof THREE.Mesh) {
+            return object;
+        }
+        const mesh = object.children.find(child => child instanceof THREE.Mesh) as THREE.Mesh | undefined;
+        if (!mesh) {
+            throw new Error("No Mesh found in object");
+        }
+        return mesh;
+    }
+}
