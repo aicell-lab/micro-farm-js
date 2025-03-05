e62a8e13d8852e6efd73ff5ac8ffbe148839d6c8
Marcus <marcus1337>
2025-03-05 10:22:35 +0100

    refactor

--- src/ctrl/tableController.ts
+++ src/ctrl/tableController.ts
@@ -3,53 +3,61 @@ import { ArmStateMachine } from '../entity/armState';
 import { ArmState, ArmCommand } from '../setup/enums';
 import { OpticsController } from './opticsController';
 import * as THREE from 'three';
-//import { Bubble } from '../entity/bubble';
 import { SelectBox } from '../entity/selectBox';
 import { Entity } from '../entity/entity';
 
+function validateTableEntity(table: Entity): void {
+    const numOptics = 10;
+    if (table.bubbles.length !== numOptics) {
+        throw new Error(`Expected exactly ${numOptics} speech buubles.`);
+    }
+    if (table.selectBoxes.length !== numOptics) {
+        throw new Error(`Expected exactly ${numOptics} selection boxes.`);
+    }
+}
+
+function initOpticsControllers(table: Entity): OpticsController[] {
+    let controllers: OpticsController[] = [];
+    for (let i = 0; i < 10; i++) {
+        const pos = new THREE.Vector3(-1.3 + i * 0.57, 1.5, -0.5);
+        if (i > 4) {
+            pos.x -= 5 * 0.57;
+            pos.z = 0.5;
+        }
+        let opticsController = new OpticsController(table.bubbles[i], pos, table.selectBoxes[i], i);
+        controllers.push(opticsController);
+    }
+    return controllers;
+}
+
+function getSlideJoint(table: Entity): URDFJoint {
+    const tableRobot = table.object as URDFRobot;
+    return tableRobot.joints["slide-j"]; // range [-3.5, 0]
+}
+
+function getSlideAngle(table: Entity): number {
+    return getSlideJoint(table).angle as number;
+}
+
+function getSlidePosition(table: Entity): THREE.Vector3 {
+    const position = new THREE.Vector3();
+    getSlideJoint(table).getWorldPosition(position);
+    return position;
+}
+
 export class TableController {
-    slideJoint: URDFJoint; // range [-3.5, 0]
     armFSM: ArmStateMachine;
     opticsControllers: OpticsController[];
     arm: Entity;
-
-
-    private checkTable(table: Entity): void {
-        const numOptics = 10;
-        if (table.bubbles.length !== numOptics) {
-            throw new Error(`Expected exactly ${numOptics} speech buubles.`);
-        }
-        if (table.selectBoxes.length !== numOptics) {
-            throw new Error(`Expected exactly ${numOptics} selection boxes.`);
-        }
-    }
-
-    private createOpticsControllers(table: Entity): OpticsController[] {
-        let controllers: OpticsController[] = [];
-        for (let i = 0; i < 10; i++) {
-            const pos = new THREE.Vector3(-1.3 + i * 0.57, 1.5, -0.5);
-            if (i > 4) {
-                pos.x -= 5 * 0.57;
-                pos.z = 0.5;
-            }
-            let opticsController = new OpticsController(table.bubbles[i], pos, table.selectBoxes[i], i);
-            controllers.push(opticsController);
-        }
-        return controllers;
-    }
+    table: Entity;
 
     constructor(table: Entity, arm: Entity) {
+        this.table = table;
         this.arm = arm;
-        this.checkTable(table);
-        const tableRobot = table.object as URDFRobot;
-        this.slideJoint = tableRobot.joints["slide-j"];
+        validateTableEntity(table);
+        this.opticsControllers = initOpticsControllers(table);
         this.armFSM = new ArmStateMachine();
-        this.opticsControllers = this.createOpticsControllers(table);
         this.setArmPosition();
-    }
-
-    getCurrentAngle(): number {
-        return this.slideJoint.angle as number;
     }
 
     public getArmState(): ArmState {
@@ -73,20 +81,19 @@ export class TableController {
     }
 
     private setArmPosition(): void {
-        let slidePos = new THREE.Vector3();
-        this.slideJoint.getWorldPosition(slidePos);
         const offset = new THREE.Vector3(-0.087, 0.0777, -0.01345);
-        this.arm.object.position.copy(slidePos.clone()).add(offset);
+        this.arm.object.position.copy(getSlidePosition(this.table).clone()).add(offset);
     }
 
     private setSlidePosition(dt: number): void {
         const speed = 1.0;
-        const currentAngle = this.getCurrentAngle();
-        const angleDifference = this.getTargetAngle() - currentAngle;
+        const angle = getSlideAngle(this.table);
+        const angleDifference = this.getTargetAngle() - angle;
         const step = Math.sign(angleDifference) * Math.min(Math.abs(angleDifference), speed * dt);
-        this.slideJoint.setJointValue(currentAngle + step);
+        const slideJoint = getSlideJoint(this.table);
+        slideJoint.setJointValue(angle + step);
         if (Math.abs(angleDifference) < 0.01) {
-            this.slideJoint.setJointValue(this.getTargetAngle());
+            slideJoint.setJointValue(this.getTargetAngle());
         }
     }
 
