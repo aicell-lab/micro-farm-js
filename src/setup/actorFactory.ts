64b9d9504e411a2331c855425105b83d02258a73
Marcus <marcus1337>
2025-03-11 14:36:49 +0100

    refactor

--- src/setup/actorFactory.ts
+++ src/setup/actorFactory.ts
@@ -99,14 +99,17 @@ export class ActorFactory {
         armObj.updateMatrixWorld(true);
     }
 
-    createArm(): Entity {
+    private buildArmObject(): Entity {
         let armRobot = Assets.getInstance().getRobots().get(Robots.Arm)!;
         const meshes = getLinkMeshMap(armRobot)
-        const options: EntityOptions = {
+        return new Entity({
             object: armRobot,
             meshes: meshes,
-        };
-        const arm = new Entity(options);
+        });
+    }
+
+    createArm(): Entity {
+        const arm = this.buildArmObject();
         this.setArmTransform(arm);
         this.colorizeArm(arm);
         return arm;
@@ -128,23 +131,40 @@ export class ActorFactory {
         return human;
     }
 
-    createOpticalTable(): Entity {
-        let tableRobot = Assets.getInstance().getRobots().get(Robots.OpticalTable)!;
-        const bubbles = Array.from({ length: 10 }, () => new Bubble());
-        const selectBoxes = Array.from({ length: 10 }, () => new SelectBox());
-        const options: EntityOptions = {
-            object: tableRobot
-        };
-        let table = new Entity(options);
-        table.bubbles = bubbles;
-        table.selectBoxes = selectBoxes;
+    private colorizeTable(table: Entity): void {
+        const obj = table.object;
+        const meshes = table.meshes;
+        applyMaterialToVisuals(obj, createMaterial, 0xfffff0);
+        const orange: number = 0xee6600;
+        setMeshColor(meshes.get("plate-microscope")!, orange);
+        setMeshColor(meshes.get("plate-incubator")!, orange);
+        setMeshColor(meshes.get("slide")!, orange);
+    }
+
+    private setTableTransform(table: Entity): void {
         table.object.position.y += 0.855;
         table.object.rotation.x = MathUtils.degToRad(270.0);
-        applyMaterialToVisuals(table.object, createMaterial, 0xfffff0);
-        getLinkMesh("plate-microscope", table.object)!.material = createMaterial(0xee6600);
-        getLinkMesh("plate-incubator", table.object)!.material = createMaterial(0xee6600);
-        getLinkMesh("slide", table.object)!.material = createMaterial(0xee6600);
         table.object.updateMatrixWorld(true);
+    }
+
+    private buildTableObject(): Entity {
+        let tableRobot = Assets.getInstance().getRobots().get(Robots.OpticalTable)!;
+        const meshes = getLinkMeshMap(tableRobot);
+        const bubbles = Array.from({ length: 10 }, () => new Bubble());
+        const selectBoxes = Array.from({ length: 10 }, () => new SelectBox());
+        const table = new Entity({
+            object: tableRobot,
+            meshes: meshes,
+        });
+        table.bubbles = bubbles;
+        table.selectBoxes = selectBoxes;
+        return table;
+    }
+
+    createOpticalTable(): Entity {
+        const table = this.buildTableObject();
+        this.setTableTransform(table);
+        this.colorizeTable(table);
         return table;
     }
 
