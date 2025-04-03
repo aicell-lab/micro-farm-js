4bfc5b73524ad389ea8182cbf146aacb2cb970fa
Marcus <marcus1337>
2025-04-03 12:38:49 +0200

    refactor

--- src/physics/physicsSystem.ts
+++ src/physics/physicsSystem.ts
@@ -17,28 +17,32 @@ function getLink(mesh: THREE.Mesh): URDFLink {
     return p2 as URDFLink;
 }
 
-export class PhysicsSystem {
-    private entities: EntityCollection;
+/*interface ArmJointState {
+    j: ArmJoints,
+    rotation: number,
+}
 
-    constructor(entities: EntityCollection) {
-        this.entities = entities;
-        this.initializePhysics();
-    }
+interface ArmState {
+    joints: ArmJointState,
+    basePosition: number,
+}
 
-    private initializePhysics(): void {
-        this.setJointAngle(ArmJoints.j0, 40);
-        this.setJointAngle(ArmJoints.j1, 10);
-        this.setJointAngle(ArmJoints.j2, 20);
-        this.setJointAngle(ArmJoints.j3, -50);
-        this.setJointAngle(ArmJoints.j4, 40);
-    }
+interface ArmTransition {
+    from: ArmState,
+    to: ArmState,
+    speed: number,
+}*/
 
-    private getArmEntity(): Entity {
-        return this.entities.getActors().arm;
+class ArmHandle {
+
+    private arm: Entity;
+
+    constructor(arm: Entity) {
+        this.arm = arm;
     }
 
     private getLinkJoint(joint: ArmJoints): URDFLink {
-        const arm = this.getArmEntity();
+        const arm = this.arm;
         switch (joint) {
             case ArmJoints.j0:
                 return getLink(arm.getMesh("arm1")!);
@@ -55,6 +59,13 @@ export class PhysicsSystem {
         }
     }
 
+    public setJointAngle(j: ArmJoints, angleDeg: number): void {
+        const angleRad = THREE.MathUtils.degToRad(angleDeg);
+        const link = this.getLinkJoint(j);
+        const axis = this.getRotationAxis(j);
+        link.rotation[axis] = angleRad;
+    }
+
     private getRotationAxis(j: ArmJoints): 'x' | 'y' | 'z' {
         switch (j) {
             case ArmJoints.j4: return 'x';
@@ -63,19 +74,32 @@ export class PhysicsSystem {
         }
     }
 
-    public setJointAngle(j: ArmJoints, angleDeg: number): void {
-        const angleRad = THREE.MathUtils.degToRad(angleDeg);
-        const link = this.getLinkJoint(j);
-        const axis = this.getRotationAxis(j);
-        link.rotation[axis] = angleRad;
+}
+
+export class PhysicsSystem {
+    private armHandle: ArmHandle;
+
+    constructor(entities: EntityCollection) {
+        this.armHandle = new ArmHandle(entities.getActors().arm);
+        this.initArm();
+    }
+
+    private initArm(): void {
+        const h = this.armHandle;
+        h.setJointAngle(ArmJoints.j0, 40);
+        h.setJointAngle(ArmJoints.j1, 10);
+        h.setJointAngle(ArmJoints.j2, 20);
+        h.setJointAngle(ArmJoints.j3, -50);
+        h.setJointAngle(ArmJoints.j4, 40);
     }
 
     public syncJoints(data: JointsSync): void {
-        this.setJointAngle(ArmJoints.j0, data.j0);
-        this.setJointAngle(ArmJoints.j1, data.j1);
-        this.setJointAngle(ArmJoints.j2, data.j2);
-        this.setJointAngle(ArmJoints.j3, data.j3);
-        this.setJointAngle(ArmJoints.j4, data.j4);
+        const h = this.armHandle;
+        h.setJointAngle(ArmJoints.j0, data.j0);
+        h.setJointAngle(ArmJoints.j1, data.j1);
+        h.setJointAngle(ArmJoints.j2, data.j2);
+        h.setJointAngle(ArmJoints.j3, data.j3);
+        h.setJointAngle(ArmJoints.j4, data.j4);
     }
 
     public step(_dt: number, _armBasePosition: THREE.Vector3): void {
